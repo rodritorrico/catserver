71bc02586d10fabb8f2ee1ffacc4a3aa3b63c0b5
whiletruedothis <rodrigo.torricodelcastillo@gmail.com>
2019-09-29 12:17:18 -0400

    configured babel

--- app.js
+++ app.js
@@ -1,41 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//import libraries------------------------
+import express from 'express';
+import path from 'path';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//import routes---------------------------
+import indexRouter from './routes/index';
 
-var app = express();
 
-// view engine setup
+// setup------------------------------------
+let app = express();
+ 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+console.log("listening to port 3000");
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+ module.exports = app;
